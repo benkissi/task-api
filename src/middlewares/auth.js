0f348121d87d98edc67fd0aa3f67fddeb31732f2
benkissi <benkissi111@gmail.com>
2019-11-26 22:20:10 +0000

    use secret variable in auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,8 +4,10 @@ const User = require('../models/users')
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
-        const decoded = jwt.verify(token, 'thesecret')
+        console.log('***token', token)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+        console.log('***user', user)
 
         if (!user) {
             throw new Error()
